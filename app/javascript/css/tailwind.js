cbf474f8c9c397a3568351cab44580e0131e4536
Emmanuelle Nadal <manue.nadal@gmail.com>
2019-12-25 17:22:58 +0100

    Design: started homepage design

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,5 +1,44 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      display: ['Klima', 'sans-serif'],
+      body: ['Noto Sans', 'sans-serif'],
+      mono: ['Space Mono', 'monospace'],
+    },
+    container: {
+      center: true,
+      padding: '0.5rem'
+    },
+    colors: {
+      blue: {
+        default: '#1E1EFF',
+        dark: '#0000E5',
+      },
+      green: {
+        default: '#5AFFB4',
+        light: '#80FFC5',
+      },
+      yellow: {
+        default: '#FFFF00',
+        light: '#FFFF9D',
+      },
+      white: '#FCFCFC',
+      black: '#030303',
+      gray: {
+          '100': '#F2F2F2',
+          '200': '#E6E6E6',
+          '300': '#B3B3B3',
+          '400': '#808080',
+          '500': '#575757',
+          '600': '#404040',
+          '700': '#333333',
+          '800': '#262626',
+          '900': '#1A1A1A',
+        }
+    },
+    borderRadius: {
+      'sm': '.2rem',
+},
     extend: {}
   },
   variants: {},
