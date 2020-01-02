64d71c3203f790046c833d8258d5d4460c6b73f3
Emmanuelle Nadal <manue.nadal@gmail.com>
2020-01-02 16:34:58 +0100

    Design: navbar (no responsive yet)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,15 @@ module ApplicationHelper
    end
    doc.to_html.html_safe
  end
- 
+
+  def navbar_button_color(navbar_mode)
+    if navbar_mode == 'black'
+      "text-white bg-black hover:bg-gray-800"
+    elsif navbar_mode == 'white'
+      "text-white bg-black hover:bg-gray-800"
+    elsif navbar_mode == 'color'
+      "text-white bg-blue hover:bg-blue-light"
+    end
+  end
+
 end
